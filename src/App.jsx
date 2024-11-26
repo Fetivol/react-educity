b0beccb3873607dae32ea086b6401b6a41c0a5ca
Fetivol <Fetisowova@gmail.com>
2024-11-26 21:51:45 +0100

    Hero + Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import ".Navbar.css";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src="" alt="" />
+    <nav className="container">
+      <img src={logo} alt="Logo" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li>
+          <button className="btn">Contact us</button>
+        </li>
+      </ul>
     </nav>
   );
 };
